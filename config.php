d04d82ee741f37577262c51ef3203ba2f368d321
jaynujangad03 <162768754+jaynujangad03@users.noreply.github.com>
2025-09-17 17:22:21 +0800

    Update config.php

--- config.php
+++ config.php
@@ -7,12 +7,9 @@ $password = getenv('DB_PASS') ?: '';
 $port = getenv('DB_PORT') ?: 3306;
 
 // Create database connection 
-try {
-// Use TCP/IP connection instead of socket 11
-$conn = new PDO("mysql:host=$host; dbname=$dbname; port=$port", $username, $password, [
-PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-PDO::ATTR_EMULATE_PREPARES => false
-
+$pdo = new PDO("mysql:host=$host;dbname=$dbname;port=$port;charset=utf8mb4", $username, $password, [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_EMULATE_PREPARES => false
 ]);
 
 } catch (PDOException $e) {
